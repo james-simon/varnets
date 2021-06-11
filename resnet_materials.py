45e506fa83cf5ed5acb0d59e4246f8153483aefe
James Simon <james.simon@berkeley.edu>
2021-06-11 15:33:29 -0700

    edited resnet support function file

--- resnet_materials.py
+++ resnet_materials.py
@@ -1,3 +1,5 @@
+import time
+
 import torch
 from torch import nn
 import torchvision
@@ -78,6 +80,11 @@ class ResNet(nn.Module):
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
 
+def kaiming_initialize(net):
+  for m in net.modules():
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+      nn.init.kaiming_normal_(m.weight)
+
 def get_cifar100_dataloader(train=True, mean=0, std=1, batch_size=128, num_workers=4, shuffle=True):
     transform_set = None
     if train:
@@ -96,3 +103,128 @@ def get_cifar100_dataloader(train=True, mean=0, std=1, batch_size=128, num_worke
     data = torchvision.datasets.CIFAR100(root='./data', train=train, download=True, transform=transform_set)
     loader = torch.utils.data.DataLoader(data, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
     return loader
+
+
+
+# https://gist.github.com/weiaicunzai/2a5ae6eac6712c70bde0630f3e76b77b
+def n_right(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = torch.sum(correct[:k].flatten()).float() #correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.item())
+    return res
+
+
+def train_epoch(net, optimizer, criterion, device, loader):
+    net.train()
+
+    epoch_loss = 0
+    epoch_acc = 0
+    epoch_acc_5 = 0
+
+    for batch_index, (X, y) in enumerate(loader):
+        X = X.to(device)
+        y = y.to(device)
+
+        y_hat = net(X)
+
+        optimizer.zero_grad()
+        loss = criterion(y_hat, y)
+        loss.backward()
+        optimizer.step()
+
+        # _, preds = outputs.max(1)
+        # correct += preds.eq(labels).sum()
+
+        # if epoch <= 1: #args.warm
+        #     warmup_scheduler.step()
+
+        batch_loss = loss.item() * X.size(0)
+        epoch_loss += batch_loss
+        batch_accs = n_right(y_hat, y, topk=(1, 5))
+        epoch_acc += batch_accs[0]
+        epoch_acc_5 += batch_accs[1]
+
+    epoch_loss /= len(loader.dataset)
+    epoch_acc /= len(loader.dataset)
+    epoch_acc_5 /= len(loader.dataset)
+    return (epoch_loss, epoch_acc, epoch_acc_5)
+
+@torch.no_grad()
+def test_epoch(net, optimizer, criterion, device, loader):
+    net.eval()
+
+    epoch_loss = 0
+    epoch_acc = 0
+    epoch_acc_5 = 0
+
+    for batch_index, (X, y) in enumerate(loader):
+        X = X.to(device)
+        y = y.to(device)
+
+        y_hat = net(X)
+
+        loss = criterion(y_hat, y)
+
+        # _, preds = outputs.max(1)
+        # correct += preds.eq(labels).sum()
+
+        batch_loss = loss.item() * X.size(0)
+        epoch_loss += batch_loss
+        batch_accs = n_right(y_hat, y, topk=(1, 5))
+        epoch_acc += batch_accs[0]
+        epoch_acc_5 += batch_accs[1]
+
+    epoch_loss /= len(loader.dataset)
+    epoch_acc /= len(loader.dataset)
+    epoch_acc_5 /= len(loader.dataset)
+    return (epoch_loss, epoch_acc, epoch_acc_5)
+
+
+def train_model(model, optimizer, train_scheduler, criterion, device, train_loader, test_loader,
+                    n_epochs, print_every_n_epochs=1):
+    # set up optimization metrics
+    train_losses = []
+    train_accs = []
+    train_accs_5 = []
+    test_losses = []
+    test_accs = []
+    test_accs_5 = []
+
+    for epoch in range(n_epochs):
+        e_start_t = time.time()
+
+        if epoch > 1:
+            train_scheduler.step(epoch)
+
+        # trainin'
+        tr_loss, tr_acc, tr_acc_5 = train_epoch(model, optimizer, criterion, device, train_loader)
+        train_losses.append(tr_loss)
+        train_accs.append(tr_acc)
+        train_accs_5.append(tr_acc_5)
+
+        # testin'
+        te_loss, te_acc, te_acc_5 = test_epoch(model, optimizer, criterion, device, test_loader)
+        test_losses.append(te_loss)
+        test_accs.append(te_acc)
+        test_accs_5.append(te_acc_5)
+
+        if epoch % print_every_n_epochs == (print_every_n_epochs - 1):
+            print(f'Epoch: {epoch}\t'
+                  f'Epoch time: {time.time() - e_start_t:.2f} --- '
+                  f'Train loss: {tr_loss:.4f}\t'
+                  f'Test loss: {te_loss:.4f}\t'
+                  f'Train accuracy: {100 * tr_acc:.2f}\t'
+                  f'(top 5): {100 * tr_acc_5:.2f}\t'
+                  f'Test accuracy: {100 * te_acc:.2f}\t'
+                  f'(top 5): {100 * te_acc_5:.2f}')
+
+    return (train_accs, train_accs_5, train_losses, test_accs, test_accs_5, test_losses)
